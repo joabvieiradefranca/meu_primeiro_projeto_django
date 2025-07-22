7e8f6855e28ac54c5dd6baa021c47c52dab855c2
Joab Vieira de França <joabvieiradefranca@gmail.com>
2025-07-22 19:48:21 -0300

    commit no projeto

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,24 @@
-from django.urls import path
+'''from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.lista_tarefas, name='lista_tarefas'),
 ]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.lista_tarefas, name='lista_tarefas'),
+]'''
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.lista_tarefas, name='lista_tarefas'),
+    path('concluir/<int:id>/', views.concluir_tarefa, name='concluir_tarefa'),
+    path('excluir/<int:id>/', views.excluir_tarefa, name='excluir_tarefa'),
+]
+
+
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+'''from django.shortcuts import render
 
 # Create your views here.
 #from django.shortcuts import render
@@ -8,4 +8,43 @@ def lista_tarefas(request):
     tarefas = Tarefa.objects.all()
     return render(request, 'todo/lista_tarefas.html', {'tarefas': tarefas})
 
+from django.shortcuts import render, redirect
+from .models import Tarefa
+
+def lista_tarefas(request):
+    if request.method == 'POST':
+        descricao = request.POST.get('descricao')
+        if descricao:
+            Tarefa.objects.create(descricao=descricao, concluida=False)
+            return redirect('lista_tarefas')  # Evita reenvio do formulário
+    tarefas = Tarefa.objects.all()
+    return render(request, 'todo/lista_tarefas.html', {'tarefas': tarefas})'''
+
+
+from django.shortcuts import render, redirect
+from .models import Tarefa
+
+def lista_tarefas(request):
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        descricao = request.POST.get('descricao')
+        if titulo:
+            Tarefa.objects.create(titulo=titulo, descricao=descricao)
+        return redirect('lista_tarefas')  # Redireciona após o envio
+
+    tarefas = Tarefa.objects.all()
+    return render(request, 'todo/lista_tarefas.html', {'tarefas': tarefas})
+
+from django.shortcuts import get_object_or_404
+
+def concluir_tarefa(request, id):
+    tarefa = get_object_or_404(Tarefa, id=id)
+    tarefa.concluida = True
+    tarefa.save()
+    return redirect('lista_tarefas')
+
+def excluir_tarefa(request, id):
+    tarefa = get_object_or_404(Tarefa, id=id)
+    tarefa.delete()
+    return redirect('lista_tarefas')
 
